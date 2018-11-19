bacd44af7d6ae41c89ba5d824f9fa5c04fa9a487
Tomotaka Yonaha <e165746@ie.u-ryukyu.ac.jp>
2018-11-19 16:52:57 +0900

    デザイン変更とリファクタリング

--- Pictorial_Time_Machine2/Saveimage.swift
+++ Pictorial_Time_Machine2/Saveimage.swift
@@ -10,84 +10,83 @@ import UIKit
 import AVFoundation
 
 class Saveimage: UIViewController {
-var img7 = UIImage(named:"iwamotoyama_ee-1200x880")!
-    var image3 = UIImage()
+  
+  var img7 = UIImage(named:"iwamotoyama_ee-1200x880")!
+  var image3 = UIImage()
   let Back_image:UIImage = UIImage(named: "Back.png")!
-    let night_image:UIImage = UIImage(named: "star3.jpg")!
-   
-    //@IBOutlet weak var gaso: UILabel!
-    @IBOutlet weak var image1: UIImageView!
+  let night_image:UIImage = UIImage(named: "star3.jpg")!
+  
+  //@IBOutlet weak var gaso: UILabel!
+  @IBOutlet weak var image1: UIImageView!
+  
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    self.view.backgroundColor = UIColor.cyan
     
+    //画像サイズ
+    image1.frame = CGRect(x: 0, y: 0, width: UIScreen.main.bounds.size.width, height: UIScreen.main.bounds.size.height)
+    image1.image = image3
     
-    override func viewDidLoad() {
-        super.viewDidLoad()
+    // Do any additional setup after loading the view.
+    let darkbutton = UIButton(type: UIButton.ButtonType.system)
+    let backbutton = UIButton()
+    let savebutton = UIButton(type: UIButton.ButtonType.system)
         
-        self.view.backgroundColor = UIColor.cyan
+    // ボタンを押した時に実行するメソッドを指定
+    darkbutton.addTarget(self, action: #selector(intheDark(_:)), for: UIControl.Event.touchUpInside)
+    backbutton.addTarget(self, action: #selector(backpage(_:)), for: UIControl.Event.touchUpInside)
+    savebutton.addTarget(self, action: #selector(Saveimg(_:)), for: UIControl.Event.touchUpInside)
         
-        //画像サイズ
-        image1.frame = CGRect(x: 0, y: 0, width: UIScreen.main.bounds.size.width, height: UIScreen.main.bounds.size.height)
+    // ラベルを設定する
+    darkbutton.setTitle("Night🌙", for: UIControl.State.normal)
+    darkbutton.setTitleColor(UIColor.white, for: UIControl.State.normal)
+    darkbutton.titleLabel!.font = UIFont.systemFont(ofSize: 25)
+      
+    backbutton.setImage(Back_image, for: .normal)
+    savebutton.setTitle("保存", for: UIControl.State.normal)
         
-        image1.image = image3
-        // Do any additional setup after loading the view.
+    // サイズ
+    darkbutton.frame = CGRect(x: 0, y: 0, width: 130, height: 80)
+    backbutton.frame = CGRect(x: 0, y: 0, width: 50, height: 50)
+    savebutton.frame = CGRect(x: 0, y: 0, width: 80, height: 80)
         
-        let graybutton = UIButton(type: UIButton.ButtonType.system)
-        let backbutton = UIButton()
-        let savebutton = UIButton(type: UIButton.ButtonType.system)
+    // 位置
+    darkbutton.layer.position = CGPoint(x: self.view.frame.width/2, y:self.view.frame.height - 45)
+    backbutton.layer.position = CGPoint(x: self.view.frame.width - 30 , y: 45)
+    savebutton.layer.position = CGPoint(x: self.view.frame.width - 45, y:self.view.frame.height - 45)
         
-        // ボタンを押した時に実行するメソッドを指定
-        graybutton.addTarget(self, action: #selector(intheDark(_:)), for: UIControl.Event.touchUpInside)
-        backbutton.addTarget(self, action: #selector(backpage(_:)), for: UIControl.Event.touchUpInside)
-        savebutton.addTarget(self, action: #selector(Saveimg(_:)), for: UIControl.Event.touchUpInside)
+    // 背景色
+    darkbutton.backgroundColor = UIColor.black.withAlphaComponent(0.6)
+    backbutton.backgroundColor = UIColor.white
+    savebutton.backgroundColor = UIColor.lightGray.withAlphaComponent(0.5)
         
-        // ラベルを設定する
-        graybutton.setTitle("夜にしちゃうぞ☆", for: UIControl.State.normal)
-        //backbutton.setTitle("✖️", for: UIControl.State.normal)
-        backbutton.setImage(Back_image, for: .normal)
-        savebutton.setTitle("保存", for: UIControl.State.normal)
+    // 枠の太さ
+    darkbutton.layer.borderWidth = 2
+    //backbutton.layer.borderWidth = 0.5
+    //savebutton.layer.borderWidth = 0.5
         
-        // サイズ
-        graybutton.frame = CGRect(x: 0, y: 0, width: 130, height: 80)
-        backbutton.frame = CGRect(x: 0, y: 0, width: 50, height: 50)
-        savebutton.frame = CGRect(x: 0, y: 0, width: 80, height: 80)
-        
-        // 位置
-        graybutton.layer.position = CGPoint(x: self.view.frame.width/2, y:self.view.frame.height - 45)
-        backbutton.layer.position = CGPoint(x: self.view.frame.width - 30 , y: 45)
-        savebutton.layer.position = CGPoint(x: self.view.frame.width - 45, y:self.view.frame.height - 45)
-        
-        // 背景色
-        graybutton.backgroundColor = UIColor(red: 0.3, green: 0.7, blue: 0.6, alpha: 1)
-        //backbutton.backgroundColor = UIColor(red: 1.0, green: 0.0, blue: 0.0, alpha: 1)
-        savebutton.backgroundColor = UIColor(red: 0.3, green: 0.7, blue: 0.6, alpha: 1)
-        
-        // 枠の太さ
-        graybutton.layer.borderWidth = 0.5
-        //backbutton.layer.borderWidth = 0.5
-        savebutton.layer.borderWidth = 0.5
-        
-        // 枠の色
-        graybutton.layer.borderColor = UIColor(red: 0.3, green: 0.6, blue: 0.5, alpha: 1).cgColor
-        //backbutton.layer.borderColor = UIColor(red: 0.3, green: 0.6, blue: 0.5, alpha: 1).cgColor
-        savebutton.layer.borderColor = UIColor(red: 0.3, green: 0.6, blue: 0.5, alpha: 1).cgColor
-        
-        // 枠に丸み
-        graybutton.layer.cornerRadius = 25
-        //backbutton.layer.cornerRadius = 25
-        savebutton.layer.cornerRadius = 25
-        
-        // viewに追加する
-        self.view.addSubview(graybutton)
-        self.view.addSubview(backbutton)
-        self.view.addSubview(savebutton)
-        
-        
-        
-    }
+    // 枠の色
+    darkbutton.layer.borderColor = UIColor.white.cgColor
+    //backbutton.layer.borderColor = UIColor.white.cgColor
+    savebutton.layer.borderColor = UIColor.white.cgColor
     
-    @objc func Saveimg(_ sender: UITapGestureRecognizer) {
-        UIImageWriteToSavedPhotosAlbum(image3, self, #selector(self.showResultOfSaveImage(_:didFinishSavingWithError:contextInfo:)), nil)
-        //gaso.text = OpenCVWrapper.openCVGasoString(image3)
-    }
+    // 枠に丸み
+    darkbutton.layer.cornerRadius = 25
+    backbutton.layer.cornerRadius = 25
+    savebutton.layer.cornerRadius = 25
+        
+    // viewに追加する
+    self.view.addSubview(darkbutton)
+    self.view.addSubview(backbutton)
+    self.view.addSubview(savebutton)
+    
+  }
+  
+  @objc func Saveimg(_ sender: UITapGestureRecognizer) {
+      UIImageWriteToSavedPhotosAlbum(image3, self, #selector(self.showResultOfSaveImage(_:didFinishSavingWithError:contextInfo:)), nil)
+      //gaso.text = OpenCVWrapper.openCVGasoString(image3)
+    
+  }
   
   // 保存を試みた結果をダイアログで表示
   @objc func showResultOfSaveImage(_ image: UIImage, didFinishSavingWithError error: NSError!, contextInfo: UnsafeMutableRawPointer){
@@ -106,12 +105,14 @@ var img7 = UIImage(named:"iwamotoyama_ee-1200x880")!
     
     // UIAlertController を表示
     self.present(alert, animated: true, completion: nil)
-  }
     
-    //ページ戻
-    @objc func backpage(_ sender: Any) {
-        self.dismiss(animated: true, completion: nil)
-    }
+  }
+  
+  //ページ戻
+  @objc func backpage(_ sender: Any) {
+      self.dismiss(animated: true, completion: nil)
+    
+  }
     
     
     /*@IBAction func toGrayScaleButtonTouched(_ sender: Any) {
@@ -142,23 +143,23 @@ var img7 = UIImage(named:"iwamotoyama_ee-1200x880")!
     
     
     */
-    @objc func intheDark(_ sender: Any) {
-        UIGraphicsBeginImageContextWithOptions(image1.image!.size, false, 0.0)
-        image1.image!.draw(in:(CGRect(x:0,y:0,width:image1.image!.size.width,height:image1.image!.size.height)))
-        image1.image = UIGraphicsGetImageFromCurrentImageContext()!
-        UIGraphicsEndImageContext()
-      
-      UIGraphicsBeginImageContextWithOptions(image3.size, false, 0.0)
-      image3.draw(in:(CGRect(x:0,y:0,width:image3.size.width,height:image3.size.height)))
-      image3 = UIGraphicsGetImageFromCurrentImageContext()!
-      UIGraphicsEndImageContext()
-      
-      
-        image3 = OpenCVWrapper.inthedark(from: image3, nightImage: night_image)
-      //image3 = OpenCVWrapper.inthedark(from: image3)
-      image1.image! = image3
-        
-    }
+  
+  @objc func intheDark(_ sender: Any) {
+    UIGraphicsBeginImageContextWithOptions(image1.image!.size, false, 0.0)
+    image1.image!.draw(in:(CGRect(x:0,y:0,width:image1.image!.size.width,height:image1.image!.size.height)))
+    image1.image = UIGraphicsGetImageFromCurrentImageContext()!
+    UIGraphicsEndImageContext()
+    
+    UIGraphicsBeginImageContextWithOptions(image3.size, false, 0.0)
+    image3.draw(in:(CGRect(x:0,y:0,width:image3.size.width,height:image3.size.height)))
+    image3 = UIGraphicsGetImageFromCurrentImageContext()!
+    UIGraphicsEndImageContext()
+    
+    image3 = OpenCVWrapper.inthedark(from: image3, nightImage: night_image)
+    //image3 = OpenCVWrapper.inthedark(from: image3)
+    image1.image! = image3
+    
+  }
     
     /*
     // MARK: - Navigation
